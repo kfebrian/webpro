7f4a06d31bc606cb741a9e03d79b194f1373ef82
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2022-11-08 17:17:27 +0700

    added search bar (homepage) and fix file structure

--- js/login.js
+++ js/login.js
@@ -20,7 +20,7 @@ async function login(){
         if (email === element.email) {
             if (password === element.password) {
                 console.log("Success");
-                window.location.href = "./index.html";
+                window.location.href = "./home.html";
             }
             else{
                 $("#password").css("border", "1px solid red");
